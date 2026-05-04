6ae773dbc37b2b716e15d1c30b0c40a085c4a454
nguyenphucishere <phucnguyenvuong07@gmail.com>
2026-05-04 04:07:47 +0000

    add eig function for eigenvalue decomposition using QR algorithm

---
+++ src/linear/eig.rs
@@ -0,0 +1,17 @@
+use crate::matrix::Matrix;
+use crate::utils::numbers::Numeric;
+use crate::linear::qr::qr;
+
+pub fn eig<N: Numeric>(A: &Matrix<N>, iterations: Option<usize>) -> (Matrix<N>, Matrix<N>) {
+    let n = A.shape().0;
+    let mut eigenvectors = Matrix::identity(n);
+    let mut eigenvalues = A.clone();
+
+    for _ in 0..iterations.unwrap_or(100) {
+        let (Q, R) = qr(&eigenvalues);
+        eigenvalues = &R * &Q;
+        eigenvectors = &eigenvectors * &Q;
+    }
+
+    (eigenvalues, eigenvectors)
+}
