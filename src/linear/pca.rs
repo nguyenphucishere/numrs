5becb5519928a047160ca53c8b06811aafc3102b
nguyenphucishere <phucnguyenvuong07@gmail.com>
2026-05-06 09:51:51 +0000

    implement randomized PCA with optional oversampling

--- src/linear/pca.rs
+++ src/linear/pca.rs
@@ -2,6 +2,7 @@ use crate::linear::svd::svd;
 use crate::matrix::Matrix;
 use crate::vector::Vector;
 use crate::utils::numbers::Numeric;
+use crate::linear::qr::qr;
 
 pub fn pca<N: Numeric>(data: Matrix<N>, n_components: usize) -> Matrix<N> {
 
@@ -56,4 +57,39 @@ pub fn pca<N: Numeric>(data: Matrix<N>, n_components: usize) -> Matrix<N> {
     // Project the data onto the selected eigenvectors
     centered_data * selected_eigenvectors
 
+}
+
+pub fn randomized_pca<N: Numeric>(mat_data: &Matrix<N>, n_components: usize, n_oversamples: usize,
+    random_data: Option<Matrix<N>>
+
+) -> Matrix<N> {
+
+    // let random_projection = Matrix::random(mat_data.shape().1, n_components, rand::random(), Some(n_oversamples));
+
+    let random_projection = if let Some(data) = random_data {
+        data
+    } else {
+        Matrix::random(mat_data.shape().1, n_components, rand::random(), Some(n_oversamples))
+    };
+
+    let projected_data = mat_data * &random_projection;
+
+    let (Q, _) = qr(&projected_data);
+    
+    let projected_data = Q.transpose() * mat_data;
+    let (_, eigenvalues, v) = svd(&projected_data);
+
+    let eigenvectors = v.transpose();
+    
+    let mut eigen_pairs: Vec<(N, Vector<N>)> = eigenvalues.main_diag().into_iter().zip(eigenvectors.col_space().vectors).collect();
+    eigen_pairs.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+
+    // Select the top n_components eigenvectors
+    let selected_eigenvectors: Matrix<N> = Matrix::from_columns(
+        &eigen_pairs.iter().take(n_components)
+        .map(|pair| pair.1.clone())
+        .collect::<Vec<Vector<N>>>()
+    );
+
+    mat_data * selected_eigenvectors
 }
