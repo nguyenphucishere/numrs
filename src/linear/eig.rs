aad1a2e4891554a1f71ba36ff5aa2b257ec0e616
nguyenphucishere <phucnguyenvuong07@gmail.com>
2026-05-06 09:51:27 +0000

    disable method 2, enable method 3

--- src/linear/eig.rs
+++ src/linear/eig.rs
@@ -113,132 +113,132 @@ pub fn eig<N: Numeric>(A: &Matrix<N>, iterations: Option<usize>) -> (Matrix<N>, 
         leading to slow convergence or even divergence in some cases.
     */
     
-        let mut eigenvectors = Matrix::identity(n);
-        let mut eigenvalues = A.clone();
-        for _ in 0..iterations.unwrap_or(10000) {
+        // let mut eigenvectors = Matrix::identity(n);
+        // let mut eigenvalues = A.clone();
+        // for _ in 0..iterations.unwrap_or(10000) {
             
-            let shift = eigenvalues[n-1][n-1];
-            let shifted_matrix = &eigenvalues - Matrix::identity(n).scale(shift);
+        //     let shift = eigenvalues[n-1][n-1];
+        //     let shifted_matrix = &eigenvalues - Matrix::identity(n).scale(shift);
             
-            let (Q, R) = qr(&shifted_matrix);
+        //     let (Q, R) = qr(&shifted_matrix);
             
-            eigenvalues = &R * &Q + Matrix::identity(n).scale(shift);
-            eigenvectors = &eigenvectors * &Q;
+        //     eigenvalues = &R * &Q + Matrix::identity(n).scale(shift);
+        //     eigenvectors = &eigenvectors * &Q;
 
-            //check for convergence
-            if (0..n-1).all(|i| eigenvalues[i+1][i].is_zero()) {
-                break;
-            }
-        }
+        //     //check for convergence
+        //     if (0..n-1).all(|i| eigenvalues[i+1][i].is_zero()) {
+        //         break;
+        //     }
+        // }
     
     // End Method 2
 
     /* Method 3 -- heissenberg reduction + QR algorithm, 
         more accurate for repeated eigenvalues, but more complex to implement and slower than method 2.
     */
-        // let max_iterations = iterations.unwrap_or(1000);
-        // let mut iter_count = 0;
-        // let (mut Q, mut H) = hessenberg_reduction(A);
+        let max_iterations = iterations.unwrap_or(1000);
+        let mut iter_count = 0;
+        let (mut Q, mut H) = hessenberg_reduction(A);
 
 
-        // let (_, n) = H.shape();
-        // let mut active_n = n;
+        let (_, n) = H.shape();
+        let mut active_n = n;
 
             
-        // while active_n > 1 {
+        while active_n > 1 {
     
-        //     // Check the bottom-most subdiagonal element.
-        //     // If it's effectively zero, the eigenvalue is isolated
+            // Check the bottom-most subdiagonal element.
+            // If it's effectively zero, the eigenvalue is isolated
     
-        //     if active_n > 1 && H[active_n - 1][active_n - 2].is_zero() {
+            if active_n > 1 && H[active_n - 1][active_n - 2].is_zero() {
                 
-        //         H[active_n - 1][active_n - 2] = N::zero();
+                H[active_n - 1][active_n - 2] = N::zero();
                 
-        //         active_n -= 1;
-        //         iter_count = 0;
-        //         continue;
-        //     }
+                active_n -= 1;
+                iter_count = 0;
+                continue;
+            }
 
-        //     if iter_count > max_iterations {
-        //         panic!("QR Algorithm failed to converge");
-        //     }
-
-
-        //     // Combined with method 2
-        //     let mu = H[active_n - 1][active_n - 1];
-        //     for i in 0..active_n {
-        //         H[i][i] = H[i][i] + N::negative() * mu;
-        //     }
-
-        //     // Store the rotations (c, s) to apply them on the right side later
-        //     // Length only needs to be active_n - 1, as we do 2x2 rotations
-        //     let mut rotations: Vec<(N, N)> = Vec::with_capacity(active_n - 1);
-
-        //     for k in 0..(active_n - 1) {
-        //         let a = H[k][k];
-        //         let b = H[k + 1][k];
-
-        //         let r = (a * a + b * b).sqrt();
-
-        //         let (c, s) = if !r.is_zero() {
-        //             (a / r, b / r)
-        //         } else {
-        //             (N::one(), N::zero())
-        //         };
-        //         rotations.push((c, s));
-
-        //         /* NOTE: We only need to update from column k to the end, 
-        //         because everything to the left is already zero */
-
-        //         for col in k..active_n {
-        //             let top_val = H[k][col];
-        //             let bot_val = H[k + 1][col];
-
-        //             H[k][col] = c * top_val + s * bot_val;
-        //             H[k + 1][col] = N::negative() * s * top_val + c * bot_val;
-        //         }
-        //     }
+            if iter_count > max_iterations {
+                panic!("QR Algorithm failed to converge");
+            }
 
 
-        //     // Apply the rotations to the columns: H = R * Q
-        //     for k in 0..(active_n - 1) {
-        //         let (c, s) = rotations[k];
+            // Combined with method 2
+            let mu = H[active_n - 1][active_n - 1];
+            for i in 0..active_n {
+                H[i][i] = H[i][i] + N::negative() * mu;
+            }
 
-        //         for row in 0..=k + 1 {
-        //             let left_val = H[row][k];
-        //             let right_val = H[row][k + 1];
+            // Store the rotations (c, s) to apply them on the right side later
+            // Length only needs to be active_n - 1, as we do 2x2 rotations
+            let mut rotations: Vec<(N, N)> = Vec::with_capacity(active_n - 1);
 
-        //             H[row][k] = c * left_val + s * right_val;
-        //             H[row][k + 1] = N::negative() * s * left_val + c * right_val;
-        //         }
+            for k in 0..(active_n - 1) {
+                let a = H[k][k];
+                let b = H[k + 1][k];
 
-        //         for row in 0..m {
-        //             let left_val = Q[row][k];
-        //             let right_val = Q[row][k + 1];
+                let r = (a * a + b * b).sqrt();
+
+                let (c, s) = if !r.is_zero() {
+                    (a / r, b / r)
+                } else {
+                    (N::one(), N::zero())
+                };
+                rotations.push((c, s));
+
+                /* NOTE: We only need to update from column k to the end, 
+                because everything to the left is already zero */
+
+                for col in k..active_n {
+                    let top_val = H[k][col];
+                    let bot_val = H[k + 1][col];
+
+                    H[k][col] = c * top_val + s * bot_val;
+                    H[k + 1][col] = N::negative() * s * top_val + c * bot_val;
+                }
+            }
+
+
+            // Apply the rotations to the columns: H = R * Q
+            for k in 0..(active_n - 1) {
+                let (c, s) = rotations[k];
+
+                for row in 0..=k + 1 {
+                    let left_val = H[row][k];
+                    let right_val = H[row][k + 1];
+
+                    H[row][k] = c * left_val + s * right_val;
+                    H[row][k + 1] = N::negative() * s * left_val + c * right_val;
+                }
+
+                for row in 0..m {
+                    let left_val = Q[row][k];
+                    let right_val = Q[row][k + 1];
         
-        //             Q[row][k] = c * left_val + s * right_val;
-        //             Q[row][k + 1] = N::negative() * s * left_val + c * right_val;
-        //         }
+                    Q[row][k] = c * left_val + s * right_val;
+                    Q[row][k + 1] = N::negative() * s * left_val + c * right_val;
+                }
 
-        //     }
+            }
 
 
-        //     // restore the shift
-        //     for i in 0..active_n {
-        //         H[i][i] += mu;
-        //     }
+            // restore the shift
+            for i in 0..active_n {
+                H[i][i] += mu;
+            }
 
-        //     iter_count += 1;
+            iter_count += 1;
 
-        // }
+        }
 
-        // /* 
-        //     TODO: Shorten the syntax for extracting eigenvalues and eigenvectors from the final H and Q matrices.
-        //     I feel like there should be a more elegant way to do this, 
-        //     but for now I'll just write it out verbosely.
-        // */
+        /* 
+            TODO: Shorten the syntax for extracting eigenvalues and eigenvectors from the final H and Q matrices.
+            I feel like there should be a more elegant way to do this, 
+            but for now I'll just write it out verbosely.
+        */
 
-        // let (eigenvalues, eigenvectors) = extract_eigensystem(&H, &Q);
+        let (eigenvalues, eigenvectors) = extract_eigensystem(&H, &Q);
 
     //End method 3
     
